fa06402336dfb882474753f8eb0e4f876a0fa7b4
drazen04 <hangtime23@hotmail.it>
2023-01-16 16:11:25 +0100

    main function: remove if statement in favor of iterating through a list of "Divisor".

--- tdd/fizz-buzz-csharp/Usings.cs
+++ tdd/fizz-buzz-csharp/Usings.cs
@@ -6,17 +6,17 @@ public class Using
     {
         Divisor divisor3 = new Divisor(3, "Buzz");
         Divisor divisor5 = new Divisor(5, "Fizz");
-        
-        var label = "";        
-        if (i % divisor5.div == 0)
-        {
-            label = divisor5.label;
-        }
 
-        if (i % divisor3.div == 0)
+        List<Divisor> divisors = new List<Divisor>() { divisor5, divisor3 };
+
+        var label = "";
+        divisors.ForEach(divisor =>
         {
-            label += divisor3.label;
-        }
+            if (i % divisor.div == 0)
+            {
+                label += divisor.label;
+            }
+        });
 
         return !String.IsNullOrEmpty(label) ? label : i.ToString();
     }
