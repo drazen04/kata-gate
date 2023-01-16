ab7ac8e5e8a29a6697b7adaf11c6afd7bdaacea7
drazen04 <hangtime23@hotmail.it>
2023-01-16 11:05:28 +0100

    tdd: create cnf-wardrobe kata.

---
+++ tdd/conf-wardrobe/src/conf-wardrobe.test.ts
@@ -0,0 +1,23 @@
+import { assert, test } from "vitest"
+
+/**
+ * Customize wardrobe by combining individual wardrobe elements.
+ * The wardrobe elements are available in the following sizes:
+ *  - 50cm,
+ *  - 75cm,
+ *  - 100cm,
+ *  - 120cm.
+ * The wall on which the wardrobe elements are placed has a total length of:
+ *  - 250cm.
+ * With which combinations of wardrobe elements can you make the most of the space?
+ * Write a function that returns all combinations of wardrobe elements that exactly fill the wall.
+ *
+ */
+
+/**
+ *
+ */
+
+test("bootstrap", () => {
+
+})
