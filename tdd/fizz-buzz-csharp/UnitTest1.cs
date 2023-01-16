4141e483e9f0d83853b35abe360a9c685654c331
drazen04 <hangtime23@hotmail.it>
2023-01-16 16:37:27 +0100

    push data up.

--- tdd/fizz-buzz-csharp/UnitTest1.cs
+++ tdd/fizz-buzz-csharp/UnitTest1.cs
@@ -8,31 +8,37 @@ public class UnitTest1
      * - [X] 15,30 - "FizzBuzz" case: n % 3 && n % 5 == 0
      * - [X] 17,97 - "17", "97" ecc. for any other case
      */
+    private static readonly List<Divisor> Divisors = new List<Divisor>()
+    {
+        new (5, "Fizz"),
+        new (3, "Buzz")
+    };
+    
     [Fact]
     public void Buzz()
     {
-        Assert.Equal("Buzz", Using.FilterMultiple(9));
-        Assert.Equal("Buzz", Using.FilterMultiple(18));
+        Assert.Equal("Buzz", Using.TransformMultiple(9, Divisors));
+        Assert.Equal("Buzz", Using.TransformMultiple(18, Divisors));
     }
     
     [Fact]
     public void Fizz()
     {
-        Assert.Equal("Fizz", Using.FilterMultiple(25));
-        Assert.Equal("Fizz", Using.FilterMultiple(50));
+        Assert.Equal("Fizz", Using.TransformMultiple(25, Divisors));
+        Assert.Equal("Fizz", Using.TransformMultiple(50, Divisors));
     }
     
     [Fact]
     public void FizzBuzz()
     {
-        Assert.Equal("FizzBuzz", Using.FilterMultiple(15));
-        Assert.Equal("FizzBuzz", Using.FilterMultiple(30));
+        Assert.Equal("FizzBuzz", Using.TransformMultiple(15, Divisors));
+        Assert.Equal("FizzBuzz", Using.TransformMultiple(30, Divisors));
     }
 
     [Fact]
     public void Num()
     {
-        Assert.Equal("17", Using.FilterMultiple(17));
-        Assert.Equal("97", Using.FilterMultiple(97));
+        Assert.Equal("17", Using.TransformMultiple(17, Divisors));
+        Assert.Equal("97", Using.TransformMultiple(97, Divisors));
     }
 }
--- tdd/fizz-buzz-csharp/Usings.cs
+++ tdd/fizz-buzz-csharp/Usings.cs
@@ -2,18 +2,13 @@ global using Xunit;
 
 public class Using
 {
-    public static string FilterMultiple(int i)
+    public static string TransformMultiple(int mul, List<Divisor> divisors)
     {
-        Divisor divisor3 = new Divisor(3, "Buzz");
-        Divisor divisor5 = new Divisor(5, "Fizz");
-
-        List<Divisor> divisors = new List<Divisor>() { divisor5, divisor3 };
-
         var label = divisors
-            .Where(divisor => IsMultipleOf(i, divisor.div))
+            .Where(divisor => IsMultipleOf(mul, divisor.div))
             .Aggregate("", (acc, divisor) => acc + divisor.label );
 
-        return !String.IsNullOrEmpty(label) ? label : i.ToString();
+        return !String.IsNullOrEmpty(label) ? label : mul.ToString();
     }
 
     public static Boolean IsMultipleOf(int num, int divisor)
