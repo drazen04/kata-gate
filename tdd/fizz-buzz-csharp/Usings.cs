6b9218457b114cc191824cc057461612499293bd
drazen04 <hangtime23@hotmail.it>
2023-01-16 16:29:58 +0100

    main function: immutable string value from divisors.

--- tdd/fizz-buzz-csharp/Usings.cs
+++ tdd/fizz-buzz-csharp/Usings.cs
@@ -9,16 +9,16 @@ public class Using
 
         List<Divisor> divisors = new List<Divisor>() { divisor5, divisor3 };
 
-        var label = "";
-        divisors.ForEach(divisor =>
-        {
-            if (i % divisor.div == 0)
-            {
-                label += divisor.label;
-            }
-        });
+        var label = divisors
+            .Where(divisor => IsMultipleOf(i, divisor.div))
+            .Aggregate("", (acc, divisor) => acc + divisor.label );
 
         return !String.IsNullOrEmpty(label) ? label : i.ToString();
+    }
+
+    public static Boolean IsMultipleOf(int num, int divisor)
+    {
+        return num % divisor == 0;
     }
 }
 
