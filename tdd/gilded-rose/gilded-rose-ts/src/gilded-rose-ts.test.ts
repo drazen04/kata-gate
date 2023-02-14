5d85395d0e3752d2b3e7f68a762cd599113a8b46
drazen04 <hangtime23@hotmail.it>
2023-02-14 10:17:34 +0100

    tdd-gilded-rose-ts: test quality and sellIn.

--- tdd/gilded-rose/gilded-rose-ts/src/gilded-rose-ts.test.ts
+++ tdd/gilded-rose/gilded-rose-ts/src/gilded-rose-ts.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { expect, it, test } from "vitest"
 
 // Gilded-Rose
 // System
@@ -24,22 +24,71 @@ import { expect, test } from "vitest";
 
 /**
  * TEST
- * [] - given one item then modify(item.quality)
- * [] - given one item then modify(item.sellIn)
- * [] - given item.quality = 0 when modify(item.quality) then item.quality = 0
- * [] - when the day ends then modify(item.sellIn) < item.sellIn
- * [] - when the day ends then modify(item.quality) < item.quality
- * [] - given sell by date passed when result = modifyX2(item.quality) then result = item.quality - (2 * lossQualityConst)
- * [] - given "Aged Brie" when modify(item.quality) then modify(item.quality) > item.quality
+ * [x] - given one item then arrange(item.quality) < item.quality
+ * [x] - given one item then arrange(item.sellIn) < item.sellIn
+ * [x] - given item.quality = 0 when arrange(item.quality) then item.quality = 0
+ * [] - when the day ends then arrange(item.sellIn) < item.sellIn
+ * [] - when the day ends then arrange(item.quality) < item.quality
+ * [] - given today = 2022/02/01, sellDate = 2022/01/30 when arrange(item.quality) then item.quality loss 2x
+ * [] - given "Aged Brie" when arrange(item.quality) then arrange(item.quality) > item.quality
  * [] - given item.quality = 50 when quality increase then item.quality = 50
- * [] - given "Sulfuras" when day ends then modify(item.quality) = item.quality
+ * [] - given "Sulfuras" when day ends then arrange(item.quality) = item.quality
  */
+type Quality = number
+type SellIn = number
 
 type Item = {
-  quality: number;
-  sellIn: number;
-};
+    name: string
+    quality: Quality
+    sellIn: SellIn
+    sellDate: Date
+}
 
-test("blaua", () => {
-  expect(2).toBe(2);
-});
+it("should lower quality and sellin", () => {
+    const item: Item = {
+        name: "salmon",
+        quality: 10,
+        sellIn: 11,
+        sellDate: new Date("2022/02/25"),
+    }
+
+    const itemResult = arrange(item)
+
+    expect(itemResult.quality).toBe(9)
+    expect(itemResult.sellIn).toBe(10)
+})
+
+it("should not lower anymore if quality is 0", () => {
+    const item: Item = {
+        name: "salmon",
+        quality: 0,
+        sellIn: 2,
+        sellDate: new Date("2022/02/16"),
+    }
+
+    const itemResult = arrange(item)
+
+    expect(itemResult.quality).toBe(0)
+})
+
+it("given quality = 0 should not lower anymore", () => {
+    const item: Item = {
+        name: "salmon",
+        quality: 0,
+        sellIn: 2,
+        sellDate: new Date("2022/02/16"),
+    }
+
+    const itemResult = arrange(item)
+
+    expect(itemResult.quality).toBe(0)
+})
+
+function arrange(item: Item): Item {
+    return {
+        name: item.name,
+        quality: item.quality > 0 ? item.quality - 1 : 0,
+        sellIn: item.sellIn - 1,
+        sellDate: item.sellDate,
+    }
+}
